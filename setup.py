daa7e99f568ff8182142b600cdc4f9aaa37927ca
CP Knight <chris@tarn.parts>
2026-04-10 21:28:33 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tarn-inventree-report-list-filters",
+    version="1.0.0",
+    description="Adds |split and |replace template filters to InvenTree reports and labels",
+    author="CP Knight",
+    author_email="chris@tarn.parts",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "inventree_plugins": [
+            "tarn_report_list_filters = tarn_inventree_report_list_filters:ListFiltersPlugin",
+        ],
+    },
+)
