c42c438354beeabb04f10542b5405f89041e7538
CP Knight <chris@tarn.parts>
2026-04-10 21:43:10 +0000

    Delete tarn_inventree_report_list_filters/apps.py

--- tarn_inventree_report_list_filters/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class TarnReportListFiltersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "tarn_inventree_report_list_filters"
-    label = "tarn_inventree_report_list_filters"
