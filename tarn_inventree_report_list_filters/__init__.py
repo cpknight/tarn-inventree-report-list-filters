c2ecde2347da815ba93e7eaf96d3469a639cdd6f
CP Knight <chris@tarn.parts>
2026-04-10 22:16:59 +0000

    Update __init__.py

--- tarn_inventree_report_list_filters/__init__.py
+++ tarn_inventree_report_list_filters/__init__.py
@@ -1,32 +1,11 @@
-from django import template
 from plugin import InvenTreePlugin
-from plugin.mixins import ReportMixin
+from plugin.mixins import AppMixin, ReportMixin
 
-register = template.Library()
-
-@register.filter
-def split(value, arg):
-    """Split a string by delimiter. Usage: {{ value|split:"|" }}"""
-    if not value:
-        return []
-    return [x.strip() for x in str(value).split(arg)]
-
-@register.filter
-def replace(value, arg1, arg2):
-    """Replace substring. Usage: {{ value|replace:"|","<br>• " }}"""
-    if not value:
-        return ""
-    return str(value).replace(arg1, arg2)
-
-class ListFiltersPlugin(InvenTreePlugin, ReportMixin):
+class ListFiltersPlugin(InvenTreePlugin, AppMixin, ReportMixin):
     NAME = "Report List Filters"
     SLUG = "tarn_report_list_filters"
     TITLE = "Report List Filters"
     DESCRIPTION = "Provides |split and |replace filters for report and label templates"
-    VERSION = "1.0.1"
+    VERSION = "1.1.0"
     AUTHOR = "CP Knight"
     AUTHOR_EMAIL = "chris@tarn.parts"
-
-    def add_report_context(self, report_instance, model_instance, request, context):
-        # Explicitly register the filters in report context
-        pass
